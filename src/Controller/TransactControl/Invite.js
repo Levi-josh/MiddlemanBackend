f1130676269ea0e37cbc9f2b84d4efbc105f93f6
Levi-josh <levijoshuakelly@gmail.com>
2024-06-05 21:41:32 -0200

    updated

--- src/Controller/TransactControl/Invite.js
+++ src/Controller/TransactControl/Invite.js
@@ -33,48 +33,104 @@ const chatdetails = (user) => ({
 })
 
 
-const sendInvite = async(req,res,next)=>{
-  const {userid,myid} = req.body
-  let check = true
-  const generatedToken = crypto.randomUUID()
-  console.log(generatedToken)
-  try {
-  const inviter = await users.findOne({_id:myid})
-    const mydetails = {
-      accept:false,
-      reject:false,
-      username:inviter.username,
-      note:`You have been invited by ${inviter.username} for a business transaction`,
-      profilePic:inviter.profilePic,
-      socketId:inviter.socketId
-  }
- await users.findOneAndUpdate({_id:userid},{$push:{notification:mydetails}})
-  while (check) {
-    const inviteduser = await users.findOne({_id:userid})
-    const choice = inviteduser.notification.filter(prev=>prev.username == inviter.username)
-    console.log(choice)
-    if (choice[0]?.accept) {
-      await users.updateOne({_id:inviter._id},{$push:{chats:chatdetails(inviteduser)}})
-      await users.updateOne({_id:userid},{$push:{chats:chatdetails(inviter)}})
-      // push to transaction also
-      await users.updateOne({_id:inviter._id},{$push:{transaction:createTransactionDetails(inviteduser._id,generatedToken)}})
-      await users.updateOne({_id:userid},{$push:{transaction:createTransactionDetails(inviter._id,generatedToken)}})
-      check = false
-      res.status(200).json({"message":'invite accepted'})
-    }
-    if (choice[0]?.reject) {
-      await users.updateOne({_id:inviter._id},{$push:{notification:`${invitedUser.username} rejected your invite`}})
-      check = false
-      res.status(200).json({"message":'invite rejected'})
-    }
-    await new Promise(resolve => setTimeout(resolve, 10000));
-  } 
-  res.status(200).json({"message":'invite sent'})
-  } catch (err) {
-    next(err)
-  }
-}
+// const sendInvite = async(req,res,next)=>{
+//   const {userid,myid} = req.body
+//   let check = true
+//   const generatedToken = crypto.randomUUID()
+//   console.log(generatedToken)
+//   try {
+//   const inviter = await users.findOne({_id:myid})
+//     const mydetails = {
+//       accept:false,
+//       reject:false,
+//       username:inviter.username,
+//       note:`You have been invited by ${inviter.username} for a business transaction`,
+//       profilePic:inviter.profilePic,
+//       socketId:inviter.socketId
+//   }
+//  await users.findOneAndUpdate({_id:userid},{$push:{notification:mydetails}})
+//   while (check) {
+//     const inviteduser = await users.findOne({_id:userid})
+//     const choice = inviteduser.notification.filter(prev=>prev.username == inviter.username)
+//     console.log(choice)
+//     if (choice[0]?.accept) {
+//       await users.updateOne({_id:inviter._id},{$push:{chats:chatdetails(inviteduser)}})
+//       await users.updateOne({_id:userid},{$push:{chats:chatdetails(inviter)}})
+//       // push to transaction also
+//       await users.updateOne({_id:inviter._id},{$push:{transaction:createTransactionDetails(inviteduser._id,generatedToken)}})
+//       await users.updateOne({_id:userid},{$push:{transaction:createTransactionDetails(inviter._id,generatedToken)}})
+//       check = false
+//       res.status(200).json({"message":'invite accepted'})
+//     }
+//     if (choice[0]?.reject) {
+//       await users.updateOne({_id:inviter._id},{$push:{notification:`${invitedUser.username} rejected your invite`}})
+//       check = false
+//       res.status(200).json({"message":'invite rejected'})
+//     }
+//     await new Promise(resolve => setTimeout(resolve, 10000));
+//   } 
+//   res.status(200).json({"message":'invite sent'})
+//   } catch (err) {
+//     next(err)
+//   }
+// }
+const sendInvite = async (req, res, next) => {
+    const { userid, myid } = req.body;
+    const generatedToken = crypto.randomUUID();
+    console.log(generatedToken);
 
+    try {
+        const inviter = await users.findOne({ _id: myid });
+        if (!inviter) {
+            return res.status(404).json({ message: 'Inviter not found' });
+        }
+
+        const mydetails = {
+            accept: false,
+            reject: false,
+            username: inviter.username,
+            note: `You have been invited by ${inviter.username} for a business transaction`,
+            profilePic: inviter.profilePic,
+            socketId: inviter.socketId
+        };
+
+        await users.findOneAndUpdate({ _id: userid }, { $push: { notification: mydetails } });
+
+        const checkNotification = async () => {
+            const invitedUser = await users.findOne({ _id: userid });
+            const choice = invitedUser.notification.find(prev => prev.username == inviter.username);
+
+            if (choice?.accept) {
+                await users.updateOne({ _id: inviter._id }, { $push: { chats: chatdetails(invitedUser) } });
+                await users.updateOne({ _id: userid }, { $push: { chats: chatdetails(inviter) } });
+                await users.updateOne({ _id: inviter._id }, { $push: { transaction: createTransactionDetails(invitedUser._id, generatedToken) } });
+                await users.updateOne({ _id: userid }, { $push: { transaction: createTransactionDetails(inviter._id, generatedToken) } });
+                return { message: 'Invite accepted' }; // to break the loop
+            }
+
+            if (choice?.reject) {
+                await users.updateOne({ _id: inviter._id }, { $push: { notification: `${invitedUser.username} rejected your invite` } });
+                return { message: 'Invite rejected' };; // to break the loop
+            }
+
+            return null;
+        };
+
+        const checkInvitations = async () => {
+          while (true) {
+              const result = await checkNotification();
+              if (result) {
+                  return res.status(200).json(result);
+              }
+              await new Promise(resolve => setTimeout(resolve, 10000)); // Polling every 10 seconds
+          }
+      };
+
+      await checkInvitations();
+    } catch (err) {
+        next(err);
+    }
+};
 const acceptInvite = async(req,res,next)=>{
   const {myid,noteId} = req.body
   try {
