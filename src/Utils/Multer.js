d494af6ffc9701b1b516c0a20e6fef377d865a71
Levi-josh <levijoshuakelly@gmail.com>
2024-09-02 11:06:33 -0200

    Revert "updated"
    This reverts commit d58446673b13188500d4557dc627a266e7204be1.

--- src/Utils/Multer.js
+++ src/Utils/Multer.js
@@ -1,6 +1,17 @@
 const multer = require('multer');
+const path = require('path');
 
-const upload = multer({ storage: multer.memoryStorage() });
+// Configure storage for multer
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/'); // Save files to 'uploads/' directory
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname)); // Generate unique filename
+  },
+});
+
+const upload = multer({ storage });
 
 module.exports = upload;
 
