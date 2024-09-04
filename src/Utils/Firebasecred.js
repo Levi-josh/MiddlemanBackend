775c0b933a8119fcec41bbf1718e1c9ebae9403a
Levi-josh <levijoshuakelly@gmail.com>
2024-09-04 09:53:18 -0200

    updated

--- src/Utils/Firebasecred.js
+++ src/Utils/Firebasecred.js
@@ -1,9 +1,9 @@
 const admin = require('firebase-admin');
-const path = require('path')
-const fs = require('fs');
+// const path = require('path')
+// const fs = require('fs');
 
-const serviceAccountPath = path.resolve(__dirname, '../../../firebasecred/Serviceaccout.json');
-const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, 'utf-8'));
+// const serviceAccountPath = path.resolve(__dirname, '../../../firebasecred/Serviceaccout.json');
+// const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, 'utf-8'));
 admin.initializeApp({
   credential: admin.credential.applicationDefault(),
   storageBucket: "middleman-5eccd.appspot.com",
