e8f6de00a4c45d8067c0be68fe25a0c0aa522195
Levi-josh <levijoshuakelly@gmail.com>
2024-09-06 07:39:43 -0200

    updated

--- src/Utils/Firebasecred.js
+++ src/Utils/Firebasecred.js
@@ -1,9 +1,4 @@
 const admin = require('firebase-admin');
-// // const path = require('path')
-// const fs = require('fs');
-
-// const serviceAccountPath = path.resolve(__dirname, '../../../firebasecred/Serviceaccout.json');
-// const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, 'utf-8'));
 
 admin.initializeApp({
   credential: admin.credential.cert(process.env.GOOGLE_APPLICATION_CREDENTIALS),
